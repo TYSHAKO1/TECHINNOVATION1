ede5583b7aa3a8f1d0a77cd3fde5e917a4c44f30
Aconnell1 <74208963+Aconnell1@users.noreply.github.com>
2022-03-01 10:54:01 -0600

    fixed some stuff

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>GPA Calc</title>
+
+</head>
+
+<body>
+
+    <center>
+    
+    </center>
+
+</body>
+
+<head>
+
+    <title>Web Development User #08</title>
+
+</head>
+
+<body>
+
+    <center>My name is User #08</center>
+
+</body>
+
+</html>
